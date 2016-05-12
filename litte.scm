e84a5327543da8e06873cc35e795f5a5b0ec5f05
Sasha Grodzins <sasha.grodzins@devmynd.com>
2016-05-12 17:03:53 -0500

    Add * functions

--- litte.scm
+++ litte.scm
@@ -114,6 +114,7 @@
 
 (display "remove all occurances of the word")
 (multirember 'wooo '(wooo where am i in the wooo planet earth wooo))
+(multirember 'wooo '((wooo where ami) in the wooo (planet wooo) earth))
 
 (define multiinsertR
   (lambda (new old lat)
@@ -285,7 +286,6 @@
 (powerOf 2 3)
 (powerOf 5 3)
 
-
 (define length
   (lambda (lat)
     (cond
@@ -310,7 +310,6 @@
 
 (rempick '4 '(Remove the number four from the list please))
 
-
 (define noNums
   (lambda (lat)
     (cond
@@ -332,3 +331,205 @@
             (else (allNums (cdr lat))))))))
 
 (allNums '(4 There are 4 words 8))
+
+(define oddsList
+  (lambda (lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+            ((odd? (car lat))
+             (cons (car lat) (oddsList (cdr lat))))
+            (else
+             (oddsList (cdr lat))))))))
+
+(oddsList '(1 2 3 4 5))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+     ((and (number? a1) (number? a2))
+      (= a1 a1))
+     ((or (number? a1) (number? a2))
+      #f)
+     (else (eq? a1 a2)))))
+
+(eqan? 'atom 'atom)
+(eqan? 'atom 'atomm)
+(eqan? 1 1)
+
+(eq? 'atom 'atom)
+(= 4 4)
+(= '4 '4)
+
+(define occurs
+  (lambda (a lat)
+    (cond
+     ((null? lat) 0)
+     (else
+      (cond
+       ((eq? (car lat) a)
+        (add1 (occurs a (cdr lat))))
+       (else (occurs a (cdr lat))))))))
+
+(occurs 'times '(How many times does times appear?))
+
+(define onelongest?
+  (lambda (n)
+    (cond
+     ((zero? n) #f)
+     (else (zero? (sub1 n))))))
+
+(define onelong?
+  (lambda n
+    (cond
+     (= n 1))))
+
+(define one?
+  (lambda n
+    (= n 1)))
+
+(one? '1)
+(one? '3)
+
+(define rempick
+  (lambda (n lat)
+    (cond
+     ((one? n) (cdr lat))
+     (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(rempick '3 '(List of three things))
+
+(define rember*
+  (lambda (a l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) a)
+        (rember* a (cdr l)))
+       (else (cons (car l)
+                   (rember* a (cdr l))))))
+     (else (cons (rember* a (car l))
+                 (rember* a (cdr l)))))))
+
+(rember* 'cup '((coffee) cup ((tea) cup) (and (hick)) cup))
+(multirember 'cup '((coffee) cup ((tea) cup) (and (hick)) cup))
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons old
+              (cons new
+                    (insertR* new old (cdr l)))))
+       (else (cons (car l) (insertR* new old (cdr l))))))
+     (else
+      (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
+
+(insertR* 'roast 'chuck '((how much (wood)) could ((a (wood) chuck)) (((chuck))) (if (a) ((wood chuck))) could chuck wood))
+
+
+(define occur*
+  (lambda (a l)
+    (cond
+     ((null? l) 0)
+     ((atom? (car l))
+      (cond
+       ((eq? a (car l))
+         (add1 (occur* a (cdr l))))
+       (else (occur* a (cdr l)))))
+     (else
+      ( + (occur* a (car l)) (occur* a (cdr l)))))))
+
+;;; what??
+(occur* 'banana '(banana (sundays are ((full) of ) ((banana)s))))
+
+
+(define subst*
+  (lambda (new old l)
+    (cond
+     ((null? l)
+      (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons new
+              (subst* new old (cdr l))))
+       (else
+        (cons (car l) (subst* new old (cdr l))))))
+     (else
+      (cons (subst* new old (car l)) (subst* new old (cdr l)))))))
+
+
+(subst* 'orange 'banana '((banana) split (banana ice) (cream (banana)) sherbert (banana) (bread) (banana brandy)))
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons new
+              (cons old
+                    (insertL* new old (cdr l)))))
+       (else (cons (car l) (insertL* new old (cdr l))))))
+     (else (cons (insertL* new old (car l)) (insertL* new old (cdr l)))))))
+
+(insertL* 'happy 'puppies '((In a world) (full) of puppies, (there are (puppies) (being) (puppies))))
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons old
+              (cons new
+                    (insertR* new old (cdr l)))))
+       (else (cons (car l) (insertR* new old (cdr l))))))
+     (else (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
+
+(insertR* 'puppies 'happy '((In a world) (full) of happy, (there are (happy) (being) (happy))))
+
+
+(define member*
+  (lambda (a l)
+    (cond
+     ((null? l) #f)
+     ((atom? (car l))
+      (or (eq? (car l) a) (member* a (cdr l))))
+     (else
+      (or (member* a (car l)) (member* a (cdr l)))))))
+
+(member? 'is '(This (is) a member?))
+(member* 'is '(This (is) a member?))
+(member* 'is '(This (issss) a member?))
+
+
+(define leftmost
+  (lambda (l)
+    (cond
+     ((atom? (car l)) (car l))
+     (else
+      (leftmost (cdr l))))))
+
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((anf (null? l1) (atom? (car l2))) #f)
+     ((null? l1) #f)
+     ((and (atom? (car l1)) (null? l1)) #f)
+     ((and (atom? (car l1)) (atom? (car l2))
+           (and (eqan? )))))))
+
+
+(eqlist? '(this is a list) '(this is a list))
+(eqlist? '((this) is a (list)) '((this) is a (list)))
+(eqlist? '(this (is a list)) '(THINGS (THINGS (THINGS))))
